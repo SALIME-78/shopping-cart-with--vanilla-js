c0cc977c2b7ad2d9271d08ad91222e317c099fb9
SALIME-78 <h.salim78.dz@gmail.com>
2024-09-23 22:36:32 +0100

    a shopping cart project with multiple features with html css and js

---
+++ js/products.js
@@ -0,0 +1,76 @@
+// List of products
+let products = [
+    {
+      id: 1,
+      title:"Lenovo X270",
+      imageUrl:"images/pc.png",
+      category:"Laptops",
+      rating:4.3,
+      price: 660,
+      manufactureDate: new Date('2021-05-12')
+  },
+  {
+      id:2,
+      title:"Salomon XT",
+      imageUrl:"images/shoes.png",
+      category:"Fashion",
+      rating:4.2,
+      price:110,
+      manufactureDate: new Date('2022-03-15')
+  },
+  {
+      id:3,
+      title:"Galaxy S22",
+      imageUrl:"images/S22-removebg-preview.png",
+      category: "Phones",
+      rating:4.5,
+      price:750,
+      manufactureDate: new Date('2020-02-06')
+  },
+  {
+      id:4,
+      title:"Linear Algebra",
+      imageUrl:"images/algebra-preview.png",
+      category:"E-Books",
+      rating:4.6,
+      price:100,
+      manufactureDate: new Date('2024-04-13')
+  },
+  {
+    id:5,
+    title:"Shoes X",
+    imageUrl:"images/shoes5-preview.png",
+    category:"Fashion",
+    rating:4.4,
+    price:220,
+    manufactureDate: new Date('2022-01-01')
+  },
+  {
+    id:6,
+    title:"Advanced Calculus",
+    imageUrl:"images/book.png",
+    category:"E-Books",
+    rating:4.8,
+    price:105,
+    manufactureDate: new Date('2024-03-04')
+  },
+  {
+    id:7,
+    title:"Galaxy S20",
+    imageUrl:"images/phone.png",
+    category:"Phones",
+    rating:4.7,
+    price:600,
+    manufactureDate: new Date('2024-07-07')
+  },
+  {
+    id:8,
+    title:"Lenovo X260",
+    imageUrl:"images/laptop-preview.png",
+    category:"Laptops",
+    rating:4.6,
+    price:550,
+    manufactureDate: new Date('2019-11-19'),
+  
+  },
+];
